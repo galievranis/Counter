8ad1eaeaed8c907057cecdea0e7d8cd8e7cd16e4
galievranis <galievdesign@gmail.com>
2025-11-08 14:50:40 +0500

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Ranis Galiev on 08.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
